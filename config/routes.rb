08c8a6c13066ca65a8035ccbe6fa2d32c14ae031
Anthony M <anthony@followapp.me>
2017-11-12 22:11:17 -0500

    change latest_reading route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :temperature_readings, only: [:index, :create]
-  get 'latest_reading', to: 'temperature_readings#latest_reading', as: :latest_temperature_reading
+  get 'temperature_readings/latest_reading', to: 'temperature_readings#latest_reading', as: :latest_temperature_reading
 end
