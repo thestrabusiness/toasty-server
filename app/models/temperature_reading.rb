d6db1f01fafa91d9e4ecd71ee825c498ea93133a
Anthony M <moffa.an@gmail.com>
2020-01-13 06:45:28 -0500

    add conversion from c to f

--- app/models/temperature_reading.rb
+++ app/models/temperature_reading.rb
@@ -1,4 +1,8 @@
 class TemperatureReading < ApplicationRecord
   validates :sensor_name, presence: true
   validates :degrees_celsius, presence: true, numericality: true
+
+  def degrees_fahrenheit
+    degrees_celsius * 9/5 + 32
+  end
 end
